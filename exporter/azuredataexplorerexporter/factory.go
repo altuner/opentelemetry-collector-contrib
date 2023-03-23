013b9cf963886f08d23d8dc0b3258ee9604ef436
Dmitry Avdeev <dmitry.avdeev@lightstep.com>
2023-03-22 17:13:00 -0700

    Upgraded azure packages

--- exporter/azuredataexplorerexporter/factory.go
+++ exporter/azuredataexplorerexporter/factory.go
@@ -72,7 +72,6 @@ func createMetricsExporter(
 	}
 	adxCfg := config.(*Config)
 	setDefaultIngestionType(adxCfg, set.Logger)
-
 	// call the common exporter function in baseexporter. This ensures that the client and the ingest
 	// are initialized and the metrics struct are available for operations
 	adp, err := newExporter(adxCfg, set.Logger, metricsType)
@@ -102,7 +101,6 @@ func createTracesExporter(
 ) (exporter.Traces, error) {
 	adxCfg := config.(*Config)
 	setDefaultIngestionType(adxCfg, set.Logger)
-
 	// call the common exporter function in baseexporter. This ensures that the client and the ingest
 	// are initialized and the metrics struct are available for operations
 	adp, err := newExporter(adxCfg, set.Logger, tracesType)
@@ -132,7 +130,6 @@ func createLogsExporter(
 ) (exp exporter.Logs, err error) {
 	adxCfg := config.(*Config)
 	setDefaultIngestionType(adxCfg, set.Logger)
-
 	// call the common exporter function in baseexporter. This ensures that the client and the ingest
 	// are initialized and the metrics struct are available for operations
 	adp, err := newExporter(adxCfg, set.Logger, logsType)
